280e1287a2c5ce8ceb86b91dab6817324bb052cc
Juan Pablo Sosa <jpasosa@gmail.com>
2016-01-22 10:49:16 -0300

    vista estática de productos

---
+++ application/frontend/productos/views/template_front/productos.php
@@ -0,0 +1,45 @@
+<div role="main" class="main">
+	<div class="slider-container rev_slider_wrapper">
+		<div id="revolutionSlider" class="slider rev_slider" data-plugin-revolution-slider data-plugin-options='{"gridwidth": 1170, "gridheight": 329}'>
+			<ul>
+				<li data-transition="fade">
+					<img src="<?php echo ASSETS . 'front/img/slides/slide-home.jpg' ?>" alt=""	data-bgposition="center center"	data-bgfit="cover" 	data-bgrepeat="no-repeat" 	class="rev-slidebg">
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<hr class="tall">
+			</div>
+		</div>
+		<div class="row">
+			<h2>Somos productores y acopiadores</h2>
+			<div class="col-sm-5">
+				<h4 class="heading-primary">
+					Coriandro /<a href="<?php echo base_url('coriando'); ?>" class="btn btn-3d btn-primary btn-xs" style="color:#ffffff">Ver m&aacute;s</a>
+				</h4>
+				<p>
+					<strong>Nuestra exportación está asegurada</strong>
+				</p>
+				<ul class="list list-icons">
+					<li><i class="fa fa-check"></i> Somos productores de la especie aromática de mayor exportación de Argentina.</li>
+					<li><i class="fa fa-check"></i> Tenemos 500 hectáreas en Carcaraña, en el sur de Santa Fé.</li>
+					<li><i class="fa fa-check"></i> Vamos a ampliar nuestra superficie productiva en un 10%.</li>
+				</ul>
+			</p>
+		</div>
+		<div class="col-sm-7">
+			<h4 class="heading-primary">Acopio de especies aromáticas</h4>
+			<p>
+				<strong>Diversificamos para reducir riesgos</strong>
+				<ul class="list list-icons">
+					<li><i class="fa fa-check"></i> Nos asociamos con los principales productores de especies aromáticas de Argentina.</li>
+					<li><i class="fa fa-check"></i> Invertimos un porcentaje de nuestro fondo de inversión en acopio de las especies con mayor demanda.</li>
+					<li><i class="fa fa-check"></i> Tenemos canales de comercialización directos con mercados nacionales e internacionales. </li>
+				</ul>
+			</p>
+		</div>
+	</div>
+</div>
